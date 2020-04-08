1b88df3930c044a4d272dc0cd607917dcfc68399
Samiran Roy <samboyadvance@Kallols-iMac.Dlink>
2020-04-09 00:08:38 +0530

    integrated hindi translation, replaced icon on the homepage with text

--- bootstrap.py
+++ bootstrap.py
@@ -24,10 +24,10 @@ Question.objects.all().delete()
 from googletrans import Translator
 translator = Translator()
 
-lang_code_translator = {"English": "en", "Hindi": "hi", "Bengali": "bn", "Marathi": "mr", "Tamil": "ta", "Telugu": "te", "Kannada": "kn", "Gujarati":"gu"}
+lang_code_translator = {"English": "en", "Hindi": "hi", "Bengali": "bn", "Marathi": "mr", "Tamil": "ta", "Telugu": "te", "Kannada": "kn", "Gujarati":"gu", "Malayalam":"ml"}
 
 
-english_questions = pd.read_excel("questions/questions_english.xlsx")
+english_questions = pd.read_excel("questions/questions.xlsx", sheet_name = "English")
 for i, row in english_questions.iterrows():
 #     print(row['question'], row['answer'])
     question = Question.objects.create(question_text = row['question'], answer_text = row['answer'], language = "English")
@@ -38,12 +38,23 @@ for i, row in english_questions.iterrows():
     print("done", row['question'])
 
 
-for i, row in english_questions.iterrows():
+hindi_questions = pd.read_excel("questions/questions.xlsx", sheet_name = "Hindi")
+for i, row in hindi_questions.iterrows():
+#     print(row['question'], row['answer'])
+    question = Question.objects.create(question_text = row['question'], answer_text = row['answer'], language = "Hindi")
+    tts = gTTS(remove_tags(row['answer']), lang='hi')
+    tts.save('corona_core/static/sounds/' + str(question.id) + '_' + 'answer.mp3')
+    tts = gTTS(remove_tags(row['question']), lang='hi')
+    tts.save('corona_core/static/sounds/' + str(question.id) + '_' + 'question.mp3')
+    print("done", row['question'])
+
+
+for i, row in hindi_questions.iterrows():
     for key in lang_code_translator.keys():
-        if key!="English":
+        if key not in ["English", "Hindi"]:
             print(key)
-            question_translated = translator.translate(row['question'], src="en", dest=lang_code_translator[key]).text
-            answer_translated = translator.translate(row['answer'], src="en", dest=lang_code_translator[key]).text
+            question_translated = translator.translate(row['question'], src="hi", dest=lang_code_translator[key]).text
+            answer_translated = translator.translate(row['answer'], src="hi", dest=lang_code_translator[key]).text
             question = Question.objects.create(question_text = question_translated, answer_text = answer_translated, language = key)
             tts = gTTS(remove_tags(answer_translated), lang=lang_code_translator[key])
             tts.save('corona_core/static/sounds/' + str(question.id) + '_' + 'answer.mp3')
