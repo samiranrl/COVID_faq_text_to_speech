c36af0ad12176f1ff7910cfc7112956ccefd508e
Samiran Roy <5029681-samiranrl@users.noreply.gitlab.com>
2020-04-13 00:40:03 +0530

    added bengali, modify about page

--- bootstrap.py
+++ bootstrap.py
@@ -89,13 +89,23 @@ for i, row in kannada_questions.iterrows():
     tts.save('corona_core/static/sounds/' + str(question.id) + '_' + 'question.mp3')
     print("done", row['question'])
 
+bengali_questions = pd.read_excel("questions/questions.xlsx", sheet_name = "Bengali")
+for i, row in bengali_questions.iterrows():
+#     print(row['question'], row['answer'])
+    question = Question.objects.create(question_text = row['question'], answer_text = row['answer'], language = "Bengali")
+    tts = gTTS(remove_tags(row['answer']), lang='bn')
+    tts.save('corona_core/static/sounds/' + str(question.id) + '_' + 'answer.mp3')
+    tts = gTTS(remove_tags(row['question']), lang='bn')
+    tts.save('corona_core/static/sounds/' + str(question.id) + '_' + 'question.mp3')
+    print("done", row['question'])
 
-for i, row in hindi_questions.iterrows():
+
+for i, row in english_questions.iterrows():
     for key in lang_code_translator.keys():
-        if key not in ["English", "Hindi", "Gujarati", "Marathi", "Malayalam", "Kannada"]:
+        if key not in ["English", "Hindi", "Gujarati", "Marathi", "Malayalam", "Kannada", "Bengali"]:
             print(key)
-            question_translated = translator.translate(row['question'], src="hi", dest=lang_code_translator[key]).text
-            answer_translated = translator.translate(row['answer'], src="hi", dest=lang_code_translator[key]).text
+            question_translated = translator.translate(row['question'], src="en", dest=lang_code_translator[key]).text
+            answer_translated = translator.translate(row['answer'], src="en", dest=lang_code_translator[key]).text
             question = Question.objects.create(question_text = question_translated, answer_text = answer_translated, language = key)
             tts = gTTS(remove_tags(answer_translated), lang=lang_code_translator[key])
             tts.save('corona_core/static/sounds/' + str(question.id) + '_' + 'answer.mp3')
